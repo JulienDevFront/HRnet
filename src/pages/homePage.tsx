593da290d53a4cee0441d06ef6ea412af09b6fa7
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-02 20:01:24 +0200

    feat(integration): integration HTML elements for the homePage page

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,3 +1,34 @@
+import styles from "../assets/styles/pages/homePage.module.scss";
+import welcome_image from "../assets/images/welcome_image.svg";
+
 export default function HomePage() {
-	return <h1>Home page</h1>;
+	return (
+		<div className={styles.homePage}>
+			<header>
+				<h1>HRnet.</h1>
+			</header>
+			<aside>
+				<img src={welcome_image} alt="welcome image" />
+			</aside>
+			<nav>
+				<button>create new employee</button>
+				<button>consult the employees</button>
+			</nav>
+			<main>
+				<section>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+					nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+					fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.
+				</section>
+				<section>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua.
+				</section>
+			</main>
+			<footer>copyright @2025 HRnet author Julien Genty</footer>
+		</div>
+	);
 }
