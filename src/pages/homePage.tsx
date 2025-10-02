e34e287875af22ef0a5617a0df86405f7c2f7754
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-02 22:23:42 +0200

    style(pages): add new style for page HomePage

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -8,25 +8,26 @@ export default function HomePage() {
 			<header>
 				<h1>HRnet.</h1>
 			</header>
-			<aside>
-				<img src={welcome_image} alt="welcome image" />
-			</aside>
 			<nav>
+				<img src={welcome_image} alt="welcome image" />
 				<Button path="/addEmploye" text="create new employee" />
 				<Button path="/listEmploye" text="consult the employees" />
 			</nav>
 			<main>
 				<section>
+					<h2>About HRnet.</h2>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
 					incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
 					nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
 					fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-					culpa qui officia deserunt mollit anim id est laborum.
-				</section>
-				<section>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-					incididunt ut labore et dolore magna aliqua.
+					culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+					amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+					dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+					sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+					minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+					commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+					esse cillum dolore eu fugiat nulla pariatur.
 				</section>
 			</main>
 			<footer>copyright @2025 HRnet author Julien Genty</footer>
