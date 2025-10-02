c61ce9a69e90e1b09f945eccb67ce80c00606cc5
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-02 23:22:12 +0200

    feat(page): apply integration and style for ErrorPage

--- src/pages/errorPage.tsx
+++ src/pages/errorPage.tsx
@@ -1,3 +1,57 @@
-export default function ErrorPage() {
-	return <h1>Error page</h1>;
+import styles from "../assets/styles/pages/errorPage.module.scss";
+import error_page from "../assets/images/error_page_image.svg";
+import { Link } from "react-router";
+import { isRouteErrorResponse, useRouteError } from "react-router";
+
+export default function ErrorBoundary() {
+	const error = useRouteError();
+
+	if (isRouteErrorResponse(error)) {
+		return (
+			<div className={styles.errorPage}>
+				<img src={error_page} alt="Image of error page" />
+				<header>
+					<h1>{error.status}</h1>
+					<h2>{error.statusText}</h2>
+				</header>
+				<footer>
+					{error.data}
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Sunt in culpa qui officia{" "}
+					<Link to="/">deserunt mollit anim id est laborum.</Link>
+				</footer>
+			</div>
+		);
+	} else if (error instanceof Error) {
+		return (
+			<div className={styles.errorPage}>
+				<img src={error_page} alt="Image of error page" />
+				<header>
+					<h1>Error</h1>
+					<h2>{error.message}</h2>
+				</header>
+				<footer>
+					{error.stack}
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Sunt in culpa qui officia{" "}
+					<Link to="/">deserunt mollit anim id est laborum.</Link>
+				</footer>
+			</div>
+		);
+	} else {
+		return (
+			<div className={styles.errorPage}>
+				<img src={error_page} alt="Image of error page" />
+				<header>
+					<h1>Error</h1>
+					<h2>form not found</h2>
+				</header>
+				<footer>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Sunt in culpa qui officia{" "}
+					<Link to="/">deserunt mollit anim id est laborum.</Link>
+				</footer>
+			</div>
+		);
+	}
 }
