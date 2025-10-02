9233178434b8f2fa475116aa46af17e5a059d90c
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-02 18:10:14 +0200

    style(pattern): add pattern 7-1 and css start

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
 import { routes } from "./routes.tsx";
+import "./assets/styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<StrictMode>
