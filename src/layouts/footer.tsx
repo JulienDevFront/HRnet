5c4d14214bd439db12d88e63431f5e55af352fb2
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-05 00:31:08 +0200

    feat(layout): add new footer component

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,3 +1,10 @@
+/**
+ * This function return a `footer` component
+ */
 export default function Footer() {
-	return <footer>copyright @2025 HRnet author Julien Genty</footer>;
+	return <footer className={styles.footer}>copyright @2025 HRnet author Julien Genty</footer>;
 }
+
+const styles = {
+	footer: "fixed bottom-0 right-0 left-0 p-4 text-center bg-neutral-800 text-neutral-200",
+};
