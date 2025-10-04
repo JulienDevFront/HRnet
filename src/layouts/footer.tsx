ddf3416055ed56a8521a9c7220fb2daf85004491
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-04 16:47:06 +0200

    feat(layout): add footer layout

---
+++ src/layouts/footer.tsx
@@ -0,0 +1,5 @@
+import styles from "../assets/styles/layouts/footer.module.scss";
+
+export default function Footer() {
+	return <footer className={styles.footer}>copyright @2025 HRnet author Julien Genty</footer>;
+}
