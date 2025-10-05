7683ed4a1c091c50861cd4ae5f11b51a4d9a4089
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-05 16:08:48 +0200

    feat(component): add new button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,6 +5,20 @@ type ButtonProps = {
 	text: string;
 };
 
+/**
+ * The component displays a button with a link `to` toward a personalized path
+ * - - -
+ * @param path type `string`
+ * @param text type `sting`
+ */
 export default function Button({ path, text }: ButtonProps) {
-	return <Link to={path}>{text}</Link>;
+	return (
+		<Link to={path} className={styles.button}>
+			{text}
+		</Link>
+	);
 }
+
+const styles = {
+	button: "block p-4 bg-neutral-800 border-2 border-neutral-800 text-center  text-neutral-300 rounded-sm hover:bg-neutral-300 hover:text-neutral-800 transition-all duration-100 ease-in-out font-bold",
+};
