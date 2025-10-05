b72f7ad9e82e380a2ec80cfd4993cfd09c243c6d
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-05 21:21:43 +0200

    feat(page): update AddEmployePage page

--- src/pages/addEmployePage.tsx
+++ src/pages/addEmployePage.tsx
@@ -1,33 +1,46 @@
 import add_employee from "../assets/add_employee_image.svg";
 import Input from "../components/input";
 import Header from "../layouts/header";
-import Footer from "../layouts/footer";
 
+/**
+ * This component return `AddEmployePage`, a page of `HRnet`
+ */
 export default function AddEmployePage() {
-	return (
-		<div>
-			<Header to="/" showClose={true} />
-			<aside>
-				<img src={add_employee} alt="Illustration" />
-			</aside>
-			<form>
-				<Input label="name" type="text" placeholder="Harry" />
-				<Input label="last name" type="text" placeholder="Potter" />
-				<Input label="email address" type="email" placeholder="HarryPotter@gmail.com" />
-				<Input label="date of birth" type="date" placeholder="" />
-				<Input label="date of start" type="date" placeholder="" />
-				<fieldset>
-					<Input label="street" type="text" placeholder="street 3/4" />
-					<Input label="city" type="text" placeholder="london" />
-					<Input label="country" type="text" placeholder="England" />
-				</fieldset>
-				<fieldset>
-					<Input label="department" type="text" placeholder="griffon d'or" />
-					<Input label="zip code" type="number" placeholder="731" />
-				</fieldset>
-				<input type="submit" value="create" />
-			</form>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className={styles.AddEmployePage}>
+      <Header to="/" showClose={true} />
+      <aside>
+        <img className={styles.img} src={add_employee} alt="Illustration" />
+      </aside>
+      <form className={styles.form}>
+        <Input label="name" type="text" placeholder="Harry" />
+        <Input label="last name" type="text" placeholder="Potter" />
+        <Input
+          label="email address"
+          type="email"
+          placeholder="HarryPotter@gmail.com"
+        />
+        <Input label="date of birth" type="date" placeholder="" />
+        <Input label="date of start" type="date" placeholder="" />
+        <fieldset>
+          <Input label="street" type="text" placeholder="street 3/4" />
+          <Input label="city" type="text" placeholder="london" />
+          <Input label="country" type="text" placeholder="England" />
+        </fieldset>
+        <fieldset>
+          <Input label="department" type="text" placeholder="griffon d'or" />
+          <Input label="zip code" type="number" placeholder="731" />
+        </fieldset>
+        <input type="submit" value="create" className={styles.inputSubmit} />
+      </form>
+    </div>
+  );
 }
+
+const styles = {
+  AddEmployePage: "w-3xl",
+  img: "m-auto",
+  form: "mb-16",
+  inputSubmit:
+    "block w-full cursor-pointer rounded-sm border-2 border-neutral-800 bg-neutral-800 p-4 text-center font-bold text-neutral-200 transition-all duration-100 ease-in-out hover:bg-neutral-600",
+};
