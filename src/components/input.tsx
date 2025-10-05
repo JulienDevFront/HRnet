bf689968723d73e0c37343bfd60a620892554b07
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-05 18:48:14 +0200

    feat(component): update Input component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,14 +1,32 @@
 type InputProps = {
-	label: string;
-	type: string;
-	placeholder: string;
+  label: string;
+  type: string;
+  placeholder: string;
 };
 
+/**
+ * this component displays a input of `type` personalized with a `placeholder` and a `label`
+ * - - -
+ * @param label type `string`
+ * @param type type `string`
+ * @param placeholder type `string`
+ */
 export default function Input({ label, type, placeholder }: InputProps) {
-	return (
-		<label>
-			{label}
-			<input type={type} placeholder={placeholder} required />
-		</label>
-	);
+  return (
+    <label className={styles.label}>
+      {label}
+      <input
+        type={type}
+        placeholder={placeholder}
+        required
+        className={styles.input}
+      />
+    </label>
+  );
 }
+
+const styles = {
+  label: "mb-8 flex flex-col gap-1 font-bold",
+  input:
+    "cursor-pointer rounded-sm border-2 border-neutral-300 bg-neutral-300 p-2 px-4 font-light outline-none focus:border-2 focus:bg-neutral-50",
+};
