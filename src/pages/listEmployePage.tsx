5d63da1b50310ce2c7c1b65181ef8ae3c6baf922
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-06 15:15:44 +0200

    feat(page): add ListEmployePage page

--- src/pages/listEmployePage.tsx
+++ src/pages/listEmployePage.tsx
@@ -1,54 +1,42 @@
 import Header from "../layouts/header";
-import consult_employee from "../assets/consult_employee_image.svg";
+import image from "../assets/consult_employee_image.svg";
+import SelectCount from "../components/selectCount";
+import InputSearch from "../components/inputSearch";
+import HeroHeader from "../layouts/heroHeader";
 
+/**
+ * This component return `ListEmployePage`, a page of `HRnet`
+ */
 export default function ListEmployePage() {
-	return (
-		<div>
-			<Header to="/" showClose={true} />
-			<aside>
-				<img src={consult_employee} alt="Illustration" />
-			</aside>
-			<section>
-				<label>
-					show
-					<input type="text" />
-					entries
-				</label>
-				<label>
-					search
-					<input type="search" />
-				</label>
-			</section>
-			<main>
-				<table>
-					<thead>
-						<tr>
-							<th>name</th>
-							<th>last</th>
-							<th>birth</th>
-							<th>start</th>
-							<th>street</th>
-							<th>city</th>
-							<th>country</th>
-							<th>dep</th>
-							<th>code</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>Harry</td>
-							<td>Potter</td>
-							<td>31-juillet-1980</td>
-							<td>1-septembre-1991</td>
-							<td>1-septembre-1991</td>
-							<td>street 3/4</td>
-							<td>London</td>
-							<td>UK</td>
-							<td>731</td>
-						</tr>
-					</tbody>
-				</table>
-			</main>
-		</div>
-	);
+  return (
+    <div className="w-3xl">
+      <Header to="/" showClose={true} />
+      <HeroHeader img={image} alt="illustration" />
+      <section className="flex justify-between px-16">
+        <SelectCount />
+        <InputSearch />
+      </section>
+      <main>
+        <table className="mt-8 min-w-full table-fixed">
+          <thead className="bg-neutral-800 text-sm text-neutral-200">
+            <tr>
+              <th className="rounded-tl-xl bg-neutral-800 p-3 text-left">
+                name
+              </th>
+              <th className="bg-neutral-800 p-3 text-left">birth</th>
+              <th className="bg-neutral-800 p-3 text-left">start</th>
+              <th className="bg-neutral-800 p-3 text-left">street</th>
+              <th className="bg-neutral-800 p-3 text-left">city</th>
+              <th className="bg-neutral-800 p-3 text-left">country</th>
+              <th className="bg-neutral-800 p-3 text-left">dep</th>
+              <th className="rounded-tr-xl bg-neutral-800 p-3 text-left">
+                code
+              </th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </main>
+    </div>
+  );
 }
