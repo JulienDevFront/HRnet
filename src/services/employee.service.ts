833ac22f16b6fb399bcd1414b32b66f2142e8233
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-07 22:11:54 +0200

    feat(service): add EMPLOYEE function in employee.service.ts

---
+++ src/services/employee.service.ts
@@ -0,0 +1,36 @@
+import { formatDate } from "./date.service";
+
+export type $EMPLOYEE = {
+  id: string;
+  name: string;
+  lastName: string;
+  email: string;
+  birthDate: string;
+  startDate: string;
+  street: string;
+  city: string;
+  country: string;
+  department: string;
+  zipCode: string;
+};
+
+/**
+ * This function return the `model of data of an employee`
+ * - - -
+ * @param `...employee` type `Object`
+ */
+export function EMPLOYEE({ ...props }): $EMPLOYEE {
+  return {
+    id: crypto.randomUUID(),
+    name: props.name,
+    lastName: props.lastName,
+    email: props.email,
+    birthDate: formatDate(props.birthDate),
+    startDate: formatDate(props.startDate),
+    street: props.street,
+    city: props.city,
+    country: props.country,
+    department: props.department,
+    zipCode: props.zipCode,
+  };
+}
