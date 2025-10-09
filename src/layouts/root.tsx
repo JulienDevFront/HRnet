55d49176396dde88b8076e7217063bcfa54c84f8
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-09 12:40:40 +0200

    style(layout): apply responsive at layout root.tsx

--- src/layouts/root.tsx
+++ src/layouts/root.tsx
@@ -2,13 +2,17 @@ import { Outlet } from "react-router";
 import Footer from "./footer";
 
 /**
- *
+ * This function return the wrapper `root` component
  */
 export default function Root() {
   return (
-    <div className="flex min-h-dvh justify-center p-16 bg-neutral-200">
+    <div className={styles.root}>
       <Outlet />
       <Footer />
     </div>
   );
 }
+
+const styles = {
+  root: "flex min-h-dvh justify-center bg-neutral-300 pb-14 max-md:px-4 md:px-8 lg:px-16",
+};
