bf9cbc0d52d4c786cc2fa432c527267596f2500d
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-09 12:12:26 +0200

    style(page): apply the responsive at page HomePage.tsx

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -12,22 +12,17 @@ export default function HomePage() {
       <Header to="/" />
       <HeroHeader img={image} alt="illustration" />
       <nav className={styles.nav}>
-        <Button path="/addEmploye" text="create new employee" />
-        <Button path="/listEmploye" text="consult the employees" />
+        <Button path="addEmployee" text="create new employee" />
+        <Button path="listEmployee" text="consult the employees" />
       </nav>
       <main className={styles.main}>
-        <section className="">
+        <section>
           <h2 className={styles.h2}>About HRnet.</h2>
           <p className={styles.p}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
         </section>
       </main>
@@ -36,11 +31,9 @@ export default function HomePage() {
 }
 
 const styles = {
-  homePage: "mb-4 flex max-w-3xl flex-1 flex-col items-center justify-center",
-  aside: "w-96",
-  nav: "w-96",
-  main: "mt-8",
-  section: "",
-  h2: "text-right leading-8 font-bold",
-  p: "text-justify leading-8",
+  homePage: "flex max-w-5xl flex-1 flex-col items-center justify-center",
+  nav: "w-full",
+  main: "mt-8 pb-8",
+  h2: "text-right leading-8 font-bold max-md:text-xs md:text-sm lg:text-base",
+  p: "text-justify leading-8 max-md:text-xs md:text-sm lg:text-base",
 };
