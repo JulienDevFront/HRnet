664f05cc0fdda20e19e781a5dc5e97a9c314fa8d
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-09 11:11:32 +0200

    style(component): apply the responsive at component Input.ts

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,7 +1,9 @@
 type InputProps = {
   label: string;
   type: string;
-  placeholder: string;
+  name: string;
+  placeholder?: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 };
 
 /**
@@ -10,23 +12,20 @@ type InputProps = {
  * @param label type `string`
  * @param type type `string`
  * @param placeholder type `string`
+ * @param onChange type `event`
  */
-export default function Input({ label, type, placeholder }: InputProps) {
+export default function Input({ label, type, name, placeholder, onChange }: InputProps) {
   return (
     <label className={styles.label}>
-      {label}
-      <input
-        type={type}
-        placeholder={placeholder}
-        required
-        className={styles.input}
-      />
+      <span className={styles.span}>{label}</span>
+      <input type={type} name={name} placeholder={placeholder} required className={styles.input} onChange={onChange} />
     </label>
   );
 }
 
 const styles = {
   label: "mb-8 flex flex-col gap-1 font-bold",
+  span: "px-4 max-md:text-xs md:text-sm lg:text-base",
   input:
-    "cursor-pointer rounded-sm border-2 border-neutral-300 bg-neutral-300 p-2 px-4 font-light outline-none focus:border-2 focus:bg-neutral-50",
+    "cursor-pointer rounded-sm border-1 border-neutral-50 bg-neutral-300 px-4 py-2 font-light outline-none focus:border-2 focus:bg-neutral-50 max-md:text-xs md:text-sm lg:text-base",
 };
