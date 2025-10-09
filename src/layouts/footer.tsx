1522d355ce30aa5653ae0a77e4296f3b59820fd1
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-10-09 12:19:16 +0200

    style(layout): apply the responsive at layout footer.tsx

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -2,9 +2,15 @@
  * This function return a `footer` component
  */
 export default function Footer() {
-	return <footer className={styles.footer}>copyright @2025 HRnet author Julien Genty</footer>;
+  return (
+    <footer className={styles.footer}>
+      <h3 className={styles.h3}>HRnet</h3> ©copyright 2025 author Julien Genty
+    </footer>
+  );
 }
 
 const styles = {
-	footer: "fixed bottom-0 right-0 left-0 p-4 text-center bg-neutral-800 text-neutral-200",
+  footer:
+    "fixed right-0 bottom-0 left-0 flex h-14 items-center justify-center gap-1 bg-neutral-800 text-center text-neutral-200 max-md:text-xs md:text-sm lg:text-base",
+  h3: "font-bold",
 };
